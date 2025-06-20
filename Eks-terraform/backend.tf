ae621056b0b475625a3090737a8316955e3f1274
Nisharg-Raval <nishargraval32@gmail.com>
2025-06-20 14:45:18 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bucketprojectt5598" # Replace with your actual S3 bucket name
+    bucket = "jenkins-ci-cd-1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
